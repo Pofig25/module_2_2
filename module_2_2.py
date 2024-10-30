2bde16fbb528c2cf4fa45f41d2fd179088681113
Pofig25 <barmindm@gmail.com>
2024-10-30 21:50:04 +1000

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -2,6 +2,8 @@ first = int(input("Enter first number: "))
 second = int(input("Enter second number: "))
 third = int(input("Enter third number: "))
 if first == second and first == third and second == third:
+#или, как вариант:
+#if first == second == third:
     print(3)
 elif first == second or first == third or second == third:
     print(2)
